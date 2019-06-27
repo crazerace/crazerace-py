d3171f3a3d1927e7ce30ac59ef4f0f3740087faf
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-06-27 21:15:46 +0200

    Added metrics collection and enpoint

--- crazerace/http/instrumentation.py
+++ crazerace/http/instrumentation.py
@@ -1,10 +1,16 @@
 # Standard library
+import time
+import sys
+from datetime import datetime
 from functools import wraps
 from uuid import uuid4
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Set, Optional
 
 # 3rd party modules.
+import flask
+import prometheus_client
 from flask import request
+from prometheus_client import Counter, Histogram, multiprocess, CollectorRegistry
 
 # Internal modules
 from crazerace import _log
@@ -12,6 +18,24 @@ from crazerace.http.error import InternalServerError
 
 
 REQUEST_ID_HEADER: str = "X-Request-ID"
+CONTENT_TYPE_LATEST = str("text/plain; version=0.0.4; charset=utf-8")
+_IGNORED_METRIC_ROUTES: Set[str] = {"/health", "/metrics"}
+
+metrics_registry = CollectorRegistry()
+multiprocess.MultiProcessCollector(metrics_registry)
+
+REQUESTS_TOTAL = Counter(
+    "http_requests_total",
+    "Service Request Count",
+    ["method", "endpoint", "http_status"],
+    registry=metrics_registry,
+)
+REQUEST_LATENCY = Histogram(
+    "request_latency_ms",
+    "Request latency in milliseconds",
+    ["method", "endpoint"],
+    registry=metrics_registry,
+)
 
 
 def add_request_id() -> None:
@@ -21,6 +45,17 @@ def add_request_id() -> None:
     _log.info(
         f"Incomming request {request.method} {request.path} requestId=[{request.id}]"
     )
+
+
+def add_request_id_to_response(response: flask.Response) -> flask.Response:
+    """Adds request id header to each response.
+
+    :param response: Response to add header to.
+    :return: Response with header.
+    """
+    response.headers[REQUEST_ID_HEADER] = request.id
+    response.headers["Date"] = f"{datetime.utcnow()}"
+    return response
 
 
 def get_request_id(fail_if_missing: bool = True) -> str:
@@ -44,3 +79,50 @@ def trace(namespace: str = "") -> Callable:
         return decorated
 
     return trace_with_namespace
+
+
+def start_timer() -> None:
+    request.start_time = time.time()
+
+
+def stop_timer(response: flask.Response) -> flask.Response:
+    if request.path not in _IGNORED_METRIC_ROUTES:
+        latency = _calculate_latency(request.start_time)
+        REQUEST_LATENCY.labels(request.method, _parse_endpoint()).observe(latency)
+    return response
+
+
+def record_request_data(response: flask.Response):
+    if request.path not in _IGNORED_METRIC_ROUTES:
+        REQUESTS_TOTAL.labels(
+            request.method, _parse_endpoint(), response.status_code
+        ).inc()
+    return response
+
+
+def _calculate_latency(start_time: float) -> float:
+    end_time = time.time()
+    milliseconds = (end_time - start_time) * 1e3
+    return round(milliseconds, 2)
+
+
+def _parse_endpoint() -> str:
+    rule = request.url_rule
+    return str(rule) if rule is not None else "NOT_FOUND"
+
+
+def setup_instrumentation(app: flask.Flask) -> None:
+    app.before_request(start_timer)
+    app.before_request(add_request_id)
+    # The order here matters since we want stop_timer
+    # to be executed first
+    app.after_request(record_request_data)
+    app.after_request(stop_timer)
+    app.after_request(add_request_id_to_response)
+
+    @app.route("/metrics")
+    def metrics():
+        return flask.Response(
+            prometheus_client.generate_latest(metrics_registry),
+            mimetype=CONTENT_TYPE_LATEST,
+        )
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="crazerace",
-    version="0.6.0",
+    version="0.7.0",
     packages=find_packages(exclude=["test*"]),
     include_package_data=True,
-    install_requires=["Flask==1.0.2", "PyJWT==1.7.1"],
+    install_requires=["Flask==1.0.2", "PyJWT==1.7.1", "prometheus_client==0.7.1"],
 )
