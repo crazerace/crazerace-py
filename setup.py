137550aed588e9564180bb3f69720cf26aaaffa9
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-12-08 19:39:20 +0100

    Added required dependencies

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="crazerace",
-    version="0.8.0",
+    version="0.9.0",
     packages=find_packages(exclude=["test*"]),
     include_package_data=True,
-    install_requires=["Flask==1.0.2", "PyJWT==1.7.1", "prometheus_client==0.7.1"],
+    install_requires=[
+        "Flask==1.1.1",
+        "PyJWT==1.7.1",
+        "prometheus_client==0.7.1",
+        "requests==2.22.0",
+    ],
 )
